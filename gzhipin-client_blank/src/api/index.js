0a037cfa9fd59808efab03d731fb1521da55d37f
Misaki <zh357964059@vip.qq.com>
2019-09-27 17:21:01 +0800

    封装ajax请求

---
+++ gzhipin-client_blank/src/api/index.js
@@ -0,0 +1,12 @@
+/**
+ * 包含n个接口请求函数的模块
+ * 返回的都是 promise
+ */
+import ajax from './ajax'
+
+// 注册接口
+export const reqRegister = (user) => ajax('/register', user, 'POST')
+// 登录接口
+export const reqLogin = ({username, password}) => ajax('/login', {username, password}, 'POST')
+// 更新用户信息接口
+export const reqUpdateUser = (user) => ajax('/update', user, 'POST')
